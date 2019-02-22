c09895e9cb23ac91907148ae306b84bd98528694
Pardeep009 <44672031+Pardeep009@users.noreply.github.com>
2019-02-22 10:20:36 +0530

    Update swap using resursion in array.cpp

--- swap using resursion in array.cpp
+++ swap using resursion in array.cpp
@@ -21,29 +21,6 @@ void f(int a[],int i,int j)
 		c++;
 	}
 }
-/*
-void f(int a[],int i,int j)
-{
-	static int c;
-	if(a[j-1]==-1)
-	{
-		c=i+1;
-		return;
-	}
-	else if(a[j]==-1)
-	{
-		c=i+1;
-		return;
-	}
-	else
-	{
-		f(a,i+1,j+2);
-		a[i]=a[i]+a[c];
-		a[c]=a[i];
-		c++;
-	}
-}
-*/
 int main()
 {
 	int n,i,j=0;
@@ -56,7 +33,6 @@ int main()
 	}
 	a[j]=-1;
 	f(a,0,0);
-//	f(a,0,2);
 	for(i=0;i<=j;i++)
 	{
 		cout<<a[i]<<" ";
