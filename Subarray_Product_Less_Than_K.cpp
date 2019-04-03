20730ee0f2eed6bc9c94b01bafa9ea1341644125
Pardeep009 <44672031+Pardeep009@users.noreply.github.com>
2019-04-03 12:42:23 +0530

    Update Subarray_Product_Less_Than_K.cpp

--- Subarray_Product_Less_Than_K.cpp
+++ Subarray_Product_Less_Than_K.cpp
@@ -20,7 +20,7 @@ public:
     {
         int size=a.size();
         long int c=0;
-        if(k==0)
+        if(k<=1)
         {
             return 0;
         }
@@ -81,3 +81,23 @@ public:
         return c;
     }
 };
+/*
+  Java Soln :->
+  class Solution 
+  {
+    public int numSubarrayProductLessThanK(int[] nums, int k) 
+    {
+        if (k <= 1) 
+          return 0;
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.length; right++) 
+        {
+            prod *= nums[right];
+            while (prod >= k) 
+              prod /= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+  }
+*/
