2d958c21166aa2ab7c7e7d16526750bebed569a0
Pardeep009 <44672031+Pardeep009@users.noreply.github.com>
2019-04-23 00:16:22 +0530

    Update and rename Cutting_a_Rod_recursive.cpp to Cutting_a_Rod_recursive(optimizatized code).cpp

--- Cutting_a_Rod_recursive(optimizatized code).cpp
+++ Cutting_a_Rod_recursive(optimizatized code).cpp
@@ -2,14 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int max_cost=INT_MIN;
-int find(int a[],int l)
+int find(int a[],int l,int b[])
 {
     if(l==0)
     return 0;
+    if(b[l])
+        return b[l];
     int max_cost=INT_MIN;
         int i;
         for(i=1;i<=l;i++)
-            max_cost=max(a[i]+find(a,l-i),max_cost);
+            max_cost=max(a[i]+find(a,l-i,b),max_cost);
+	b[l]=max_cost;
     return max_cost;
 }
 int main() 
@@ -17,10 +20,11 @@ int main()
     int n;
     cin>>n;       // enter the length of rod
     int a[n+1];
+    int *b=new int[n+1];
     for(int i=1;i<=n;i++)
     {
         cin>>a[i];       // cost of cutting of rod for each length
     }
-    cout<<find(a,n);       // max profit
+    cout<<find(a,n,b);       // max profit
 	return 0;
 }
