f2bf35119aefc335bbb9778066ce9d08d9c776a6
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2019-07-29 12:56:51 +0530

    Update Maximize Number of 1's By Flipping M zeroes.cpp

--- Maximize Number of 1's By Flipping M zeroes.cpp
+++ Maximize Number of 1's By Flipping M zeroes.cpp
@@ -1,3 +1,25 @@
+/*
+	Input:   arr[] = {1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1}
+         m = 2
+	Output:  5 7
+	We are allowed to flip maximum 2 zeroes. If we flip
+	arr[5] and arr[7], we get 8 consecutive 1's which is
+	maximum possible under given constraints 
+
+	Input:   arr[] = {1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1}
+		 m = 1
+	Output:  7
+	We are allowed to flip maximum 1 zero. If we flip 
+	arr[7], we get 5 consecutive 1's which is maximum 
+	possible under given constraints.
+
+	Input:   arr[] = {0, 0, 0, 1}
+		 m = 4
+	Output:  0 1 2
+	Since m is more than number of zeroes, we can flip
+	all zeroes.
+	
+*/
 #include<iostream>
 using namespace std;
 
