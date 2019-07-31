9ed5c77a5e6f8c2b8968c28eaba4401b249145ae
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2019-08-01 00:44:55 +0530

    Update First Missing Positive.cpp

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -1,4 +1,5 @@
 /*
+https://www.interviewbit.com/problems/first-missing-integer/
 First Missing Positive
 
 Given an unsorted integer array, find the smallest missing positive integer.
@@ -16,17 +17,39 @@ Input: [7,8,9,11,12]
 Output: 1
 
 */
-class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) 
+int Solution::firstMissingPositive(vector<int> &arr) {
+    int size = arr.size(),i=0,j=0;
+    // if(size == 1)
+    // return arr[0] == 1 ? 2 : 1;
+    while(i<size)
     {
-        vector<int>::iterator it;
-        int i;
-        for(i=1;;i++)
+        if(arr[i]<=0)
         {
-            it=find(nums.begin(),nums.end(),i);
-            if(it==nums.end())
-            return i;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j++;
+        }
+        i++;
+    }
+    if(j==size && arr[j-1]<=0)
+    {
+        return 1;
+    }
+    int index,start=j;
+    for(;j<size;j++)
+    {
+        index = abs(arr[j])-1;
+        if(index+start<size)
+        {
+            if(arr[index+start]>0)
+            arr[index+start] *=-1;
         }
     }
-};
+    for(j=start;j<size;j++)
+    {
+        if(arr[j]>0)
+        return j-start+1;
+    }
+    return j-start+1;
+}
