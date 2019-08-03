c7335eb2ab3b6222e0219654a90c2c705b513559
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2019-08-03 16:05:59 +0530

    Update Check For Binary Search Tree.cpp

--- Tree/Check For Binary Search Tree.cpp
+++ Tree/Check For Binary Search Tree.cpp
@@ -12,17 +12,14 @@
         }
     }; 
 */
-
 bool check(Node *root,int min,int max)
 {
     if(root == NULL)
     return true;
-    if(root->data>=min && root->data<=max && check(root->left,min,root->data) && check(root->right,root->data,max))  // check root element should lie in a range of max and min
-                                                                                                                       // now,check same for left subtree and right subtree
-    return true;
+    if(root->data>=min && root->data<=max && check(root->left,min,root->data) && check(root->right,root->data,max))  // check root element should lie in a range of max and min,now,check same for left subtree and right subtree
+    return true;      // bst
     else
-    return false;
-    
+    return false;      // not bst
 }
 
 bool isBST(Node* root) {
