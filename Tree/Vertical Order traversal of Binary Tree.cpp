01aca7facb59293986227ca0b834302c985b0192
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2019-08-07 23:32:42 +0530

    Update Vertical Order traversal of Binary Tree.cpp

--- Tree/Vertical Order traversal of Binary Tree.cpp
+++ Tree/Vertical Order traversal of Binary Tree.cpp
@@ -1,3 +1,27 @@
+/*
+    Problem Link:-> https://www.interviewbit.com/problems/vertical-order-traversal-of-binary-tree/
+    Given a binary tree, return a 2-D array with vertical order traversal of it.
+    Go through the example for more details.
+
+    Example :
+    Given binary tree:
+            
+          6
+        /   \
+       3     7
+      / \     \
+     2   5     9
+     
+     returns                      
+     [
+        [2],
+        [3],
+        [6 5],
+        [7],
+        [9]
+     ]  
+     
+*/
 vector<vector<int>> Solution::verticalOrderTraversal(TreeNode* A) {
     map<int,vector<int>> m;
     queue<pair<TreeNode*,int>> q;
